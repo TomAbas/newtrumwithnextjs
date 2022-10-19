6906e038b85818ce030c80d52b3b6c3cc754dcbd
TomAbas <tructam37@gmail.com>
2022-10-19 14:39:26 +0700

    click in contact

--- components/Recuitment/Recuitment.js
+++ components/Recuitment/Recuitment.js
@@ -15,6 +15,11 @@ import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import { ListItemButton, ListItemIcon } from "@mui/material";
 const Recuitment = () => {
+  const [activeSelectOption, setActiveSelectOption] = useState(false);
+	const [activeSelectOption1, setActiveSelectOption1] = useState(false);
+	const [activeSelectOption2, setActiveSelectOption2] = useState(false);
+	const [activeSelectOption3, setActiveSelectOption3] = useState(false);
+	const [activeSelectOption4, setActiveSelectOption4] = useState(false);
   const [open,setOpen] = useState(false)
   const [companyInfo, setCompanyInfo] = useState();
   const [listJob, setListJob] = useState([]);
@@ -30,13 +35,63 @@ const Recuitment = () => {
   };
   const fetchListJob = async () => {
     await axios.get(urlListJobs).then(({ data }) => {
-      console.log(data);
+      // console.log(data);
       setListJob(data);
     });
   };
-  const handleClick = () => {
-    setOpen(!open);
-  };
+
+  const showActiveSelect = (e) => {
+    // console.log(e)
+		switch (e.currentTarget.dataset.idx) {
+			case '0':
+        // console.log(0)
+        setActiveSelectOption(!activeSelectOption)
+				// setActiveSelectOption1(false);
+				// setActiveSelectOption2(false);
+				// setActiveSelectOption3(false);
+				// setActiveSelectOption4(false);
+				break;
+			case '1':
+        // console.log(1)
+				setActiveSelectOption1(!activeSelectOption1);
+        // setActiveSelectOption(false)
+				// setActiveSelectOption2(false);
+				// setActiveSelectOption3(false);
+				// setActiveSelectOption4(false);
+				break;
+			case '2':
+        // console.log(2)
+				setActiveSelectOption2(!activeSelectOption2);
+        // setActiveSelectOption(false)
+				// setActiveSelectOption1(false);
+				// setActiveSelectOption3(false);
+				// setActiveSelectOption4(false);
+				break;
+			case '3':
+        // console.log(3)
+				setActiveSelectOption3(!activeSelectOption3);
+        // setActiveSelectOption(false)
+				// setActiveSelectOption2(false);
+				// setActiveSelectOption4(false);
+				// setActiveSelectOption1(false);
+				break;
+        case '4':
+          // console.log(4)
+          setActiveSelectOption4(!activeSelectOption4);
+          // setActiveSelectOption(false)
+          // setActiveSelectOption2(false);
+          // setActiveSelectOption3(false);
+          // setActiveSelectOption1(false);
+          break;
+			default:
+        setActiveSelectOption(false)
+				setActiveSelectOption4(false);
+				setActiveSelectOption2(false);
+				setActiveSelectOption3(false);
+				setActiveSelectOption1(false);
+				break;
+		}
+	};
   const createListJobItem = () => {
     let a;
 
@@ -45,25 +100,59 @@ const Recuitment = () => {
               </div> */}
     if (listJob) {
       a = listJob.map((job, idx) => {
-        console.log(job);
+        // console.log(job);
         return (
           
           <>
-          <ListItemButton sx={{borderTop:'0.1px solid rgba(128, 128, 128, 0.298)'}} onClick={handleClick}>
+          <ListItemButton sx={{borderTop:'0.1px solid rgba(128, 128, 128, 0.298)'}} onClick={(e)=>showActiveSelect(e)} data-idx={idx}>
+
             <ListItemText primary={job.title} className={styles.item} key={idx} />          
             <ListItemIcon>
-             <AddIcon sx={{color:'#858585'}}/>
+             <AddIcon sx={{color:'#858585'}} />
           </ListItemIcon>
-          {/* {open ? <ExpandLess /> : <ExpandMore />} */}
+   
       
           </ListItemButton>
-          <Collapse in={open} timeout="auto" unmountOnExit>
+
+          {idx === 0 ? (<Collapse in={activeSelectOption} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           
-            <ListItemText primary="Starred" />
+            <ListItemText primary={job.content} sx={{padding: "8px 16px"}} />
         
         </List>
-      </Collapse>
+      </Collapse>)  : idx === 1 ? (<Collapse in={activeSelectOption1} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          
+            <ListItemText primary={job.content} sx={{padding: "8px 16px"}} />
+        
+        </List>
+      </Collapse>) : idx === 2  ? (<Collapse in={activeSelectOption2} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          
+            <ListItemText primary={job.content} sx={{padding: "8px 16px"}} />
+        
+        </List>
+      </Collapse>) : idx ===3 ? (<Collapse in={activeSelectOption3} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          
+            <ListItemText primary={job.content} sx={{padding: "8px 16px"}} />
+        
+        </List>
+      </Collapse>) :  (<Collapse in={activeSelectOption4} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          
+            <ListItemText primary={job.content} sx={{padding: "8px 16px"}} />
+        
+        </List>
+      </Collapse>) }
+
+          {/* <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          
+            <ListItemText primary={job.content} sx={{padding: "8px 16px"}} />
+        
+        </List>
+      </Collapse> */}
           </>
         );
       });
