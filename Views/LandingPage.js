5b7baeafcfd4b59c9b3807afabd9423a9ce843a2
TomAbas <tructam37@gmail.com>
2023-03-03 01:49:28 +0700

    rewrite landingpage1 logic render anim

--- Views/LandingPage.js
+++ Views/LandingPage.js
@@ -6,25 +6,21 @@ import Page03 from "../components/landingpage/LandingPage/Page-03/Page03";
 import Page04 from "../components/landingpage/LandingPage/Page-04/Page04";
 
 const LandingPage = ({ data }) => {
-	const [landingPageData, setLandingPageData] = useState({});
-	const [page1Data, setPage1Data] = useState({});
-	const [page3Data, setPage3Data] = useState({});
-	const [page4Data, setPage4Data] = useState({});
-	const [page44Data, setPage44Data] = useState({});
-	const [page444Data, setPage444Data] = useState({});
+  const [landingPageData, setLandingPageData] = useState([]);
+  const [page1Data, setPage1Data] = useState([]);
+  const [page3Data, setPage3Data] = useState([]);
+  const [page4Data, setPage4Data] = useState([]);
 
-	const setData = () => {
-		setLandingPageData(data[0]);
-		setPage1Data(data[1]);
-		setPage3Data(data[2]);
-		setPage4Data(data[3]);
-		setPage44Data(data[4]);
-		setPage444Data(data[5]);
-	};
-	console.log(data)
-	useEffect(() => {
-		setData();
-	}, []);
+  const setData = () => {
+    setLandingPageData(data.title);
+    setPage1Data(data.description);
+    setPage3Data(data.subTitle);
+    setPage4Data(data.listContent);
+  };
+
+  useEffect(() => {
+    setData();
+  }, []);
 
   return (
     <>
@@ -32,11 +28,7 @@ const LandingPage = ({ data }) => {
       <Page01 page1Data={page1Data} />
       <Page02 isLandingPage={true} />
       <Page03 page3Data={page3Data} />
-      <Page04
-        page4Data={page4Data}
-        page44Data={page44Data}
-        page444Data={page444Data}
-      />
+      <Page04 page4Data={page4Data} />
     </>
   );
 };
--- components/landingpage/LandingPage/LandingPage1/LandingPage.js
+++ components/landingpage/LandingPage/LandingPage1/LandingPage.js
@@ -4,81 +4,48 @@ import { useRef } from "react";
 import { useEffect } from "react";
 import landingpagepic from "/public/imgs/landingpagepic.png";
 const LandingPage = ({ landingPageData }) => {
-  const [line01, setLine01] = useState([]);
-  const [line02, setLine02] = useState([]);
-  const [line03, setLine03] = useState([]);
   const refWord0 = useRef();
-
-  const produceArray = () => {
-    if (landingPageData) {
-      // let firstLine = JSON.stringify(landingPageData.firstLine);
-      // let secondLine = JSON.stringify(landingPageData.secondLine);
-      // let thirdLine = JSON.stringify(landingPageData.thirdLine);
-      let firstLine = "We are expert in...";
-      let secondLine = "turning your brand into";
-      let thirdLine = "the catchy icon";
-      if (firstLine) {
-        let arrayWords = firstLine.replace(/"/g, "").split(" ");
-        setLine01(arrayWords);
-        let arrayWords1 = secondLine.replace(/"/g, "").split(" ");
-        setLine02(arrayWords1);
-        let arrayWords2 = thirdLine.replace(/"/g, "").split(" ");
-        setLine03(arrayWords2);
-      }
-    }
-  };
-  const animationWords = () => {
-    let b = line01.map((word, idx) => {
-      let delay = { animationDelay: `${idx / 8 + 0.5}s` };
+  const renderAnim = () => {
+    return landingPageData?.map((item, oIdx) => {
       return (
-        <h1 key={idx} className={styles.fadeInUp0} style={delay} ref={refWord0}>
-          {word}
-        </h1>
+        <>
+          <div className={styles.wrapperEffect}>
+            {item.content.split(" ").map((word, idx) => {
+              let delay = { animationDelay: `${oIdx / 2 + idx / 10}s` };
+              if (item.effect.indexOf(idx) !== -1) {
+                return (
+                  <h1
+                    key={idx}
+                    className={styles.fadeInUp0}
+                    style={delay}
+                    ref={refWord0}
+                  >
+                    <span>{word}</span>
+                  </h1>
+                );
+              } else {
+                return (
+                  <h1
+                    key={idx}
+                    className={styles.fadeInUp0}
+                    style={delay}
+                    ref={refWord0}
+                  >
+                    {word}
+                  </h1>
+                );
+              }
+            })}
+          </div>
+        </>
       );
     });
-    console.log(b);
-    return b;
   };
-  const animationWords1 = () => {
-    let b = line02.map((word, idx) => {
-      let delay = { animationDelay: `${idx / 12 + 1.3}s` };
-      return (
-        <h1 key={idx} className={styles.fadeInUp0} style={delay} ref={refWord0}>
-          {idx === 0 ? <span>{word}</span> : word}
-        </h1>
-      );
-    });
-    return b;
-  };
-  const animationWords2 = () => {
-    let b = line03.map((word, idx) => {
-      let delay = { animationDelay: `${idx / 15 + 1.6}s` };
-      return (
-        <h1 key={idx} className={styles.fadeInUp0} style={delay} ref={refWord0}>
-          {idx === 1 ? (
-            <span>{word}</span>
-          ) : idx === 2 ? (
-            <span>{word}</span>
-          ) : (
-            word
-          )}
-        </h1>
-      );
-    });
-    return b;
-  };
-  useEffect(() => {
-    produceArray();
-  }, [landingPageData]);
   return (
     <>
       <div className={styles.test}>
         <div className={styles.landingPageContainer}>
-          <div className={styles.loganContainer}>
-            <div className={styles.wrapperEffect}>{animationWords()}</div>
-            <div className={styles.wrapperEffect}>{animationWords1()}</div>
-            <div className={styles.wrapperEffect}>{animationWords2()}</div>
-          </div>
+          <div className={styles.loganContainer}>{renderAnim()}</div>
         </div>
       </div>
 
