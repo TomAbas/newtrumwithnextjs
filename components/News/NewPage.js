dd474994f9f8d6ca41ce20cdb37dc29b3488e375
Tom <tam.vn@tribox.me>
2023-11-02 23:32:06 +0700

    create daily-news route

--- components/News/NewPage.js
+++ components/News/NewPage.js
@@ -3,7 +3,6 @@ import styles from '../../styles/NewsPage.module.css';
 import NewPage1 from './NewPage1/NewPage1';
 import NewPage2 from './NewPage2/NewPage2';
 import NewPage3 from './NewPage3/NewPage3';
-import NewPage4 from './NewPage4/NewPage4';
 
 const NewPage = () => {
   return (
@@ -12,7 +11,6 @@ const NewPage = () => {
       <div className={styles.newPageSubContainer}>
         <NewPage2 />
         <NewPage3 />
-        <NewPage4 />
       </div>
     </div>
   )
